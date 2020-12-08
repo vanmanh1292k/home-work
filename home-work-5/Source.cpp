6ba20a53b19cd9299e299740081bfdaabe8c3a17
vanmanh1292k <ngvmanh129@gmail.com>
2020-12-08 23:11:47 +0300

    up date home wwork 5

--- home-work-5/Source.cpp
+++ home-work-5/Source.cpp
@@ -1,19 +1,54 @@
-#include<iostream>
-
-using namespace std;
-
-int euclid(int a, int b)
-{
-	return (b == 0) ? a : euclid(a, a % b);
-}
-
-
-int main() {
-	int a, b;
-	cin >> a >> b;
-	cout << "Greatest common divisor: " << euclid(a, b) << endl;
-
-	system("pause");
-	return 0;
-}
+//#include<iostream>
 //
+//using namespace std;
+//
+////Algorithm Eculide
+//int euclid(int a, int b)
+//{
+//
+//	return (b == 0) ? a : euclid(b, a % b);
+//}
+//
+//int main()
+//{
+//	int a, b;
+//	cout << "Please input first number: ";
+//	cin >> a;
+//	cout << "Please input second number: ";
+//	cin >> b;
+//
+//	cout << "Greatest common divisor: " << euclid(a, b) << endl;
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	bool a[100];
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i <= n; i++) {
+//		a[i] = true;
+//	}
+//
+//	for (int i = 2; i <= n; i++) {
+//		if (a[i] == true) {
+//			for (int j = 2 * i; j <= n; j += i) {
+//				a[j] = false;
+//			}
+//		}
+//	}
+//
+//	for (int i = 2; i <= n; i++) {
+//		if (a[i] == true) {
+//			cout << i << " ";
+//		}
+//	}
+//	system("pause");
+//}
