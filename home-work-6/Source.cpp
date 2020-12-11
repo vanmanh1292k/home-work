2cf0242258df39a1ad07105b646de37ac78c8d8b
vanmanh1292k <ngvmanh129@gmail.com>
2020-12-11 14:38:58 +0300

    up date hw-6-3

--- home-work-6/Source.cpp
+++ home-work-6/Source.cpp
@@ -58,6 +58,7 @@
 
 
 
+//// Задачи для подготовки к экзамену. файлы.
 //#include<iostream>
 //#include<stdio.h>
 //#include<fstream>
@@ -124,4 +125,20 @@
 //}
 
 
-
+//// Задачи для подготовки к экзамену.Структурные числа. Числа Фибоначчи .
+//#include <iostream>
+//using namespace std;
+//int Fibonacci(int n)
+//{
+//    if (n == 1 || n == 2)
+//        return 1;
+//    return Fibonacci(n - 1) + Fibonacci(n - 2);
+//}
+//int main()
+//{
+//    int n;
+//    cout << "nhap n: ";
+//    cin >> n;
+//    cout << "F" << n << " : " << Fibonacci(n);
+//    return 0;
+//}
