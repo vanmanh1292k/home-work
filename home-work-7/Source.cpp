dda9efb81904e167d1f5015ff0cdcf4b6d553b04
vanmanh1292k <ngvmanh129@gmail.com>
2020-12-14 11:50:49 +0300

    up date hw 7

--- home-work-7/Source.cpp
+++ home-work-7/Source.cpp
@@ -1,1 +1,64 @@
-//sadfaf
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//
+//	int A, B, C;
+//	int blade = 0;
+//
+//	cout << "enter original price A: ";
+//	cin >> A;
+//	cout << "enter the price of 1 blade B : ";
+//	cin >> B;
+//	cout << "enter the maximum cost C : ";
+//	cin >> C;
+//	for (int i = 1; i < 100; i++) {
+//		if (A + B * i <= C) {
+//			blade++;
+//		}
+//	}
+//	cout << "the maximum number of blades: " << blade << endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//
+//	int M, A[2];
+//	cout << "Please input number of blade: ";
+//	cin >> M;
+//
+//	int check = 0;
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		for (int j = 0; j < 10; j++)
+//		{
+//			if (3 * i + 4 * j == M)
+//			{
+//				cout << "Case " << ++check << endl;
+//				cout << "Number spinner have 3 blade: " << i << endl;
+//				cout << "Number spinner have 4 blade: " << j << endl;
+//
+//			}
+//
+//		}
+//	}
+//
+//	if (check == 0)
+//	{
+//		cout << "Number spinner have 3 blade: 0" << endl;
+//		cout << "Number spinner have 4 blade: 0" << endl;
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
